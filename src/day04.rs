cd88306977bb1488ccdbd06aeddd2b33dc0db9ca
Brendan Bates <batesbrendan@gmail.com>
2019-01-04 15:35:07 +1100

    With working parser for all events

--- src/day04.rs
+++ src/day04.rs
@@ -6,32 +6,47 @@ use parse;
 enum GuardEvent {
     BeginShift(GuardRecord),
     Sleep(GuardRecord),
-    WakeUp(GuardRecord)
+    WakeUp(GuardRecord),
 }
 
 #[derive(PartialEq, Eq, Debug)]
 struct GuardRecord {
     ts: NaiveDateTime,
-    guard: String
+    guard: String,
 }
 
 impl GuardEvent {
     pub fn parse(record_text: &str) -> Result<GuardEvent, String> {
+        GuardEvent::parse_for_guard(String::from(""), record_text)
+    }
+
+    pub fn parse_for_guard(guard: String, record_text: &str) -> Result<GuardEvent, String> {
         lazy_static! {
         static ref RE: Regex = Regex::new(r"^\[([\d\-: ]+)\] ((falls asleep)|(wakes up)|(Guard #(\d+) begins shift))$").unwrap();
     }
 
         if RE.is_match(record_text) {
             let cap = RE.captures(record_text).unwrap();
+            println!("{:?}", cap);
 
-            match NaiveDateTime::parse_from_str(cap.get(1).unwrap().as_str(), "%Y-%m-%d %H:%M") {
-                Ok(dt) => Ok(GuardEvent::BeginShift(
-                    GuardRecord {
-                        ts: NaiveDateTime::parse_from_str(cap.get(1).unwrap().as_str(), "%Y-%m-%d %H:%M").unwrap(),
-                        guard: String::from(cap.get(6).unwrap().as_str())
+            if let Ok(dt) = NaiveDateTime::parse_from_str(cap.get(1).unwrap().as_str(), "%Y-%m-%d %H:%M") {
+                let guard_id = if guard.is_empty() { String::from(cap.get(6).unwrap().as_str()) } else { guard };
+                let guard_record = GuardRecord {
+                    ts: dt,
+                    guard: guard_id,
+                };
+                let event_desc = cap.get(2).unwrap().as_str();
+                Ok(
+                    if event_desc == "falls asleep" {
+                        GuardEvent::Sleep(guard_record)
+                    } else if event_desc == "wakes up" {
+                        GuardEvent::WakeUp(guard_record)
+                    } else {
+                        GuardEvent::BeginShift(guard_record)
                     }
-                )),
-                Err(e) => Err(format!("Could not parse guard event '{}'", record_text).to_string())
+                )
+            } else {
+                Err(format!("Could not parse guard event datetime '{}'", record_text).to_string())
             }
         } else {
             return Err(format!("Could not parse guard event '{}'", record_text).to_string());
@@ -46,28 +61,44 @@ mod guard_event_tests {
     #[test]
     fn parse_begin_shift_guard_event() {
         assert_eq!(GuardEvent::parse("[1518-11-01 00:00] Guard #10 begins shift").unwrap(),
-        GuardEvent::BeginShift(GuardRecord {
-            ts: NaiveDate::from_ymd(1518, 11, 1).and_hms(0, 0, 0),
-            guard: String::from("10")
-        }));
+                   GuardEvent::BeginShift(GuardRecord {
+                       ts: NaiveDate::from_ymd(1518, 11, 1).and_hms(0, 0, 0),
+                       guard: String::from("10"),
+                   }));
     }
 
     #[test]
     fn parse_invalid_event_should_produce_err_result() {
         assert_eq!(GuardEvent::parse("[1518-13-01 00:00] Guard #10 begins shift"),
-                   Err(String::from("Could not parse guard event '[1518-13-01 00:00] Guard #10 begins shift'")));
+                   Err(String::from("Could not parse guard event datetime '[1518-13-01 00:00] Guard #10 begins shift'")));
 
         assert_eq!(GuardEvent::parse("[1518-11-01 00:00] Gah, not an event!"),
                    Err(String::from("Could not parse guard event '[1518-11-01 00:00] Gah, not an event!'")));
     }
+
+
+    #[test]
+    fn parse_sleep_event() {
+        assert_eq!(GuardEvent::parse_for_guard(String::from("10"), "[1518-11-01 00:00] falls asleep").unwrap(),
+                   GuardEvent::Sleep(GuardRecord {
+                       ts: NaiveDate::from_ymd(1518, 11, 1).and_hms(0, 0, 0),
+                       guard: String::from("10"),
+                   }));
+    }
+
+    #[test]
+    fn parse_wakeup_event() {
+        assert_eq!(GuardEvent::parse_for_guard(String::from("10"), "[1518-11-01 00:00] wakes up").unwrap(),
+                   GuardEvent::WakeUp(GuardRecord {
+                       ts: NaiveDate::from_ymd(1518, 11, 1).and_hms(0, 0, 0),
+                       guard: String::from("10"),
+                   }));
+    }
 }
-
-
 
 
 pub fn solve_part_one(input: &String) -> String {
     let lines = parse::split_input_lines(input);
-
 
 
     let guard = 10;
