a50de1d3a5da65da6606dbd61aace5f85e2f50c2
Brendan Bates <batesbrendan@gmail.com>
2019-01-08 21:02:29 +1100

    Non-working solution for day04

--- src/day04.rs
+++ src/day04.rs
@@ -1,6 +1,9 @@
-use chrono::{NaiveDateTime, NaiveDate};
+use chrono::{NaiveDateTime, NaiveDate, Timelike};
 use regex::Regex;
 use parse;
+use std::collections::HashMap;
+use std::ops::Range;
+use std::str::FromStr;
 
 #[derive(PartialEq, Eq, Debug)]
 enum GuardEvent {
@@ -8,6 +11,17 @@ enum GuardEvent {
     Sleep(GuardRecord),
     WakeUp(GuardRecord),
 }
+
+impl GuardEvent {
+    pub fn record(&self) -> &GuardRecord {
+        match self {
+            GuardEvent::BeginShift(x) => x,
+            GuardEvent::Sleep(x) => x,
+            GuardEvent::WakeUp(x) => x
+        }
+    }
+}
+
 
 #[derive(PartialEq, Eq, Debug)]
 struct GuardRecord {
@@ -27,10 +41,13 @@ impl GuardEvent {
 
         if RE.is_match(record_text) {
             let cap = RE.captures(record_text).unwrap();
-            println!("{:?}", cap);
 
             if let Ok(dt) = NaiveDateTime::parse_from_str(cap.get(1).unwrap().as_str(), "%Y-%m-%d %H:%M") {
-                let guard_id = if guard.is_empty() { String::from(cap.get(6).unwrap().as_str()) } else { guard };
+                let parsed_guard = match cap.get(6) {
+                    Some(m) => String::from(m.as_str()),
+                    None => String::new()
+                };
+                let guard_id = if parsed_guard.is_empty() { guard } else { parsed_guard };
                 let guard_record = GuardRecord {
                     ts: dt,
                     guard: guard_id,
@@ -96,14 +113,81 @@ mod guard_event_tests {
     }
 }
 
+type GuardSleepRanges = HashMap<String, Vec<Range<u32>>>;
+
+fn guard_sleep_times(guard_events: &Vec<GuardEvent>) -> GuardSleepRanges {
+    let mut guard_sleep_times = HashMap::<String, Vec<Range<u32>>>::new();
+
+    guard_events.iter().fold(None, |previous_event, event| {
+        let sleep_record = guard_sleep_times.entry(event.record().guard.to_string()).or_insert(Vec::<Range<u32>>::new());
+        match event {
+            GuardEvent::Sleep(_) => Some(event),
+            GuardEvent::WakeUp(gr) => {
+                sleep_record.push(previous_event.unwrap().record().ts.minute()..gr.ts.minute());
+                None
+            }
+            _ => None
+        }
+    });
+
+    guard_sleep_times
+}
+
+fn guard_with_most_times_asleep(sleep_times: &GuardSleepRanges) -> (String, &Vec<Range<u32>>) {
+    let mut sleep_times_flattened = sleep_times.iter().collect::<Vec<(&String, &Vec<Range<u32>>)>>();
+    sleep_times_flattened.sort_by_key(|(_, sleep_ranges)| -> u32 {
+        (*sleep_ranges).iter().map(|range| {
+            (range.end-1) - range.start
+        }).sum()
+    });
+    let (guard, sleep_ranges) = sleep_times_flattened.last().unwrap();
+    (guard.to_string(), &sleep_ranges.clone())
+}
+
+fn minute_most_asleep(sleep_ranges: &Vec<Range<u32>>) -> u32 {
+    let mut minute_counts = HashMap::<u32, u32>::new();
+    for range in sleep_ranges.iter() {
+        let r: Vec<u32> = range.clone().collect();
+        for m in r {
+           let ctr =  minute_counts.entry(m).or_insert(0);
+            *ctr += 1;
+        }
+    }
+    *minute_counts.iter().max_by_key(|c| { (*c).1 }).unwrap().0
+}
+
+#[derive(PartialEq, Eq, Debug)]
+struct Message {
+    ts: NaiveDateTime,
+    message: String,
+}
 
 pub fn solve_part_one(input: &String) -> String {
     let lines = parse::split_input_lines(input);
+    let (_, mut guard_events) =
+        lines.iter().fold((String::new(), Vec::<GuardEvent>::new()), |(current_guard, mut events_acc), line| {
+            let event = if current_guard.is_empty() {
+                GuardEvent::parse(line)
+            } else {
+                GuardEvent::parse_for_guard(current_guard, line)
+            }.expect(format!("Could not parse guard event! '{}'", line).as_str());
+            let next_guard = event.record().guard.to_string();
 
+            events_acc.push(event);
 
-    let guard = 10;
-    let minute_most_slept_during = 24;
-    (guard * minute_most_slept_during).to_string()
+            (next_guard, events_acc)
+        });
+    // TODO: need to split parser into two phases - one to parse timestamps, then the next one to parse the message
+
+    guard_events.sort_by_key(|k| k.record().ts);
+    println!("{:?}", guard_events.iter().take(10));
+
+    let sleep_times = guard_sleep_times(&guard_events);
+    let (guard , sleep_ranges) = guard_with_most_times_asleep(&sleep_times);
+
+    let minute_most_slept_during = minute_most_asleep(sleep_ranges);
+
+    (u32::from_str(&guard).unwrap() * minute_most_slept_during).to_string()
 }
 
 #[cfg(test)]
@@ -135,6 +219,6 @@ mod part_one_tests {
 }
 
 
-pub fn solve_part_two(input: &String) -> String {
+pub fn solve_part_two(_input: &String) -> String {
     String::from("")
 }
